eb599922ce7a268aca4ba9a3aaec659ca6c10218
Chris Emmery <cmry@protonmail.com>
2021-05-05 13:40:48 +0200

    fixed regex

--- get_reddit.py
+++ get_reddit.py
@@ -20,7 +20,9 @@ def find_gender_in_reddit(reddit_object):
     _id, text = reddit_object['id'], reddit_object['body']
     sents = re.split(r'\.|\?|\!', text)
     for sent in sents:
-        if re.findall('[^A-Za-z0-9]+[sS]*[Hh]e[^A-Za-z0-9]+', sent):
+        if re.findall(
+          r'(?:[^A-Za-z0-9]+)([sS]*[Hh])(e|is|er|im|[^A-Za-z0-9]){1}' +
+          r'(self|s)*[^A-Za-z0-9]', sent):
             yield _id, preprocess_sent(sent)
 
 
@@ -45,10 +47,17 @@ def reddit_gender(f):
     for reddit_object in iter_file(f):
         for _id, sent in find_gender_in_reddit(reddit_object):
             csv_writer.writerow([_id, sent])
+            # print(_id, sent)
 
 
 if __name__ == '__main__':
-    # test_sent = "Hey! I thought he was lame & he thought she was the best."
-    # print(re.findall('[^A-Za-z0-9]{1}[sS]*[Hh]e[^A-Za-z0-9]{1}', test_sent))
-    with Pool(1) as p:
+    test_sent = ["Hi! I thought he was lame & he thought she was the best.",
+                 "This has a his and hers, so she can wash her face while" +
+                 "he gives himself a shave there ."]
+    # for sent in test_sent:
+    #     print(sent)
+    #     print(re.findall(
+    #             r'(?:[^A-Za-z0-9]+)([sS]*[Hh])(e|is|er|im|[^A-Za-z0-9])+' +
+    #             r'(self|s)*[^A-Za-z0-9]', sent))
+    with Pool(10) as p:
         p.map(reddit_gender, glob('/data/reddit/*2019*'))
